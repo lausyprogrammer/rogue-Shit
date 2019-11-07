ada4f39ebb62d49cd8f61f02ffd479528ea198f5
Sam Lau <samuel.kwun.lau@gmail.com>
2019-11-06 19:36:07 -0500

    Updates

---
+++ precache-manifest.222a6ebdd75754fc1aad1940ca71aaed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fecd273dc17e431ce1c57e769a0aed00",
+    "url": "/rogue-Shit/index.html"
+  },
+  {
+    "revision": "3153d67bfb0995424203",
+    "url": "/rogue-Shit/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c664e6b4578a90115d9f",
+    "url": "/rogue-Shit/static/js/2.7693e6bf.chunk.js"
+  },
+  {
+    "revision": "3153d67bfb0995424203",
+    "url": "/rogue-Shit/static/js/main.eadf0102.chunk.js"
+  },
+  {
+    "revision": "ad575d5b47d87ec0b545",
+    "url": "/rogue-Shit/static/js/runtime-main.4d1f1f92.js"
+  }
+]);
